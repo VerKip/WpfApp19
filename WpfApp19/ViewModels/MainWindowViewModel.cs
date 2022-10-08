be7486d3eecedbdaf225503745618115ced2d79f
Vera Kiprskaya <vera.malyutina@gmail.com>
2022-10-09 02:31:48 +0300

    Добавьте файлы проекта.

---
+++ WpfApp19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace WpfApp19.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number2 = Number1 * Math.PI;
+        }
+
+        private bool CanAddCommandExecuted(object d)
+        {
+            if (Number1 != 0)
+                return true;
+            else return false;
+        }
+
+        public MainWindowViewModel()
+        { 
+        AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+
+    }
+}
